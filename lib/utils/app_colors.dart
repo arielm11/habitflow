fb6c46962429ec41b7fe5000120ddaa6f8bb3634
Ariel Morais <arielmorais644@gmail.com>
2025-09-27 16:38:05 -0300

    chore: Add Colors Project
    Adicionando arquivo para padronizar as cores do projeto

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Cor primária
+  static const Color teal = Color(0xFF008080);
+
+  // Cor secundária
+  static const Color seaGreen = Color(0xFF2E8B57);
+
+  // Cor de destaque
+  static const Color softGold = Color(0xFFFFD700);
+
+  // Cor de fundo
+  static const Color background = Color(0xFFF5F5DC);
+
+  // Cor de texto e ícones
+  static const Color graphite = Color(0xFF2F4F4F);
+}
