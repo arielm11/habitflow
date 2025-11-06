0c7d2d70993ebbc0d356457cd047f305ff94e935
Ariel Morais <arielmorais644@gmail.com>
2025-11-06 20:22:57 -0300

    chore: Update Config about Notification

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -15,5 +15,14 @@ import flutter_local_notifications
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  } 
+
+  @available(iOS 10.0, *)
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    willPresent notification: UNNotification,
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
+    completionHandler([.badge, .sound, .alert])
   }
 }
